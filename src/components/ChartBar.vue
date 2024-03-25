f88c61624ba21df8e847e747a62cfc70045f2025
buenomoreto <morettofelype@gmail.com>
2024-03-25 11:30:33 -0300

    fix: charbar mobile

--- src/components/ChartBar.vue
+++ src/components/ChartBar.vue
@@ -3,7 +3,8 @@ import { Bar } from 'vue-chartjs'
 import { Records } from '@/types/interface/records'
 import { data, options } from '@/config/chartBar'
 import { watchEffect } from 'vue'
-
+import { useDevice } from '@/composables/useDevice'
+const { windowSize } = useDevice()
 const props = defineProps(['records'])
 
 watchEffect(() => {
@@ -12,11 +13,15 @@ watchEffect(() => {
   data.value.datasets[1].data = props.records.map((item: Records) => item.churn.total)
 })
 </script>
-
 <template>
-  <div class="bg-primary-50 p-4 2xl:p-8 rounded-2xl mb-8 flex flex-col">
+  <div class="bg-primary-50 p-4 2xl:p-8 rounded-2xl mb-8 flex flex-col overflow-auto">
     <h1 class="text-xl text-white mb-6">Métricas MRR & Churn range</h1>
-    <Bar :data="data" :options="options" v-if="records.length" />
+    <div class="w-[1180px] overflow-scroll" v-if="windowSize.width < 1024 && records.length">
+      <Bar :data="data" :options="options" />
+    </div>
+    <div v-else-if="windowSize.width > 1024 && records.length">
+      <Bar :data="data" :options="options" />
+    </div>
     <div v-else class="text-base text-white">Nenhum dado encontrado.</div>
   </div>
 </template>
